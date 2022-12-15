789b42f78472c2a77bf2d6719b7afd3b393835fa
Tokunbo <tokzy04@gmail.com>
2022-12-15 18:53:57 +0000

    Task Update

---
+++ _queue.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * _queue - onverts stacks to a queue.
+ * @stack: double pointer to the head of stack.
+ * @line_number: script line number
+ *
+ * Return: No return.
+ */
+void _queue(stack_t **stack, unsigned int line_number)
+{
+	(void)(stack);
+	(void)(line_number);
+	var.queue = QUEUE;
+}
